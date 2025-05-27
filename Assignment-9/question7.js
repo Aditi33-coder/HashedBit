37a0533d456d699d2a27b26480c44ba861d875cd
Aditi Verma <139349539+Aditi33-coder@users.noreply.github.com>
2025-05-27 12:56:34 +0530

    Delete Assignment-9/question7.js

--- Assignment-9/question7.js
+++
@@ -1,8 +0,0 @@
-// Function to calculate the area of a rectangle
-function getRectangleArea(rectangle) {
-    return rectangle.width * rectangle.height;
-}
-
-// Example usage
-const rectangle = { width: 10, height: 5 };
-console.log(getRectangleArea(rectangle));  
